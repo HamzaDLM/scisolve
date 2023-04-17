50fc29172cbfd01ba60717a8d106a3822c4a7a59
HamzaDLM <hamzadellam@hotmail.com>
2023-04-17 03:00:06 +0000

    trying out gum

--- main.go
+++ main.go
@@ -1,68 +1,197 @@
-// Use cobra cli or bubbletea
 package main
 
 import (
 	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/muesli/reflow/indent"
+	"github.com/muesli/termenv"
 )
 
-func high_pass_filter_calculator() {
-	fmt.Println("\n[High Pass Filter Calculator]")
-	fmt.Println("Resistance (R)")
+// Define dict for domains and calculations available
+var questionsStore = map[string][]string{
+	"Math":    []string{"Question1", "Question2"},
+	"Biology": []string{"Question1", "Question2"},
+	"Physics": []string{"foiezfjeio"},
 }
 
-func fraction_to_percent_calculator() {
-	fmt.Println("Fraction to percent")
+// Secodn approach to defining the domains and questions
+// 1: "Math"
+// 2: "Biology"
+
+// func (domain) {
+// 	switch {
+
+// 	case "Math"
+// 		return ["dfeo", "ifjefi"]
+// 	}
+// }
+
+// General stuff for styling the view
+var (
+	term    = termenv.EnvColorProfile()
+	keyword = makeFgStyle("211")
+	subtle  = makeFgStyle("241")
+	dot     = colorFg(" • ", "236")
+	title   = makeFgBgStyle("#ffffff", "#341fd1")
+)
+
+type model struct {
+	Choice       int
+	ChosenDomain bool
+	ChosenCalc   bool
+	Loaded       bool
+	Quitting     bool
 }
 
-func decimal_to_percent_calculator() {
-	fmt.Println("Decimal to percent")
+func (m model) Init() tea.Cmd {
+	return tea.EnterAltScreen
+}
+
+// Main update function.
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	// Make sure these keys always quit
+	if msg, ok := msg.(tea.KeyMsg); ok {
+		k := msg.String()
+		if k == "q" || k == "esc" || k == "ctrl+c" {
+			m.Quitting = true
+			return m, tea.Quit
+		}
+	}
+
+	// Hand off the message and model to the appropriate update function for the
+	// appropriate view based on the current state.
+	if !m.ChosenDomain {
+		return updateChoices(msg, m)
+	}
+	return updateChosen(msg, m)
+}
+
+// The main view, which just calls the appropriate sub-view
+func (m model) View() string {
+	var s string
+	if m.Quitting {
+		return "\n  See you later!\n\n"
+	}
+	if !m.ChosenDomain {
+		s = choicesView(m)
+	} else {
+		s = chosenView(m)
+	}
+	return indent.String("\n"+s+"\n\n", 2)
+}
+
+// Sub-update functions
+
+// Update loop for the first view where you're choosing a task.
+func updateChoices(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		// TODO: make the length check dynamic
+		case "j", "down":
+			m.Choice++
+			if m.Choice > 3 {
+				m.Choice = 3
+			}
+		case "k", "up":
+			m.Choice--
+			if m.Choice < 0 {
+				m.Choice = 0
+			}
+		case "enter":
+			m.ChosenDomain = true
+			return m, nil
+		}
+	}
+	return m, nil
+}
+
+// Update loop for the second view after a choice has been made
+func updateChosen(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
+	switch msg.(type) {
+	}
+	return m, nil
+}
+
+// Sub-views
+
+// The first view, for choosing the domain
+func choicesView(m model) string {
+	c := m.Choice
+
+	tpl := title("Welcome to SciSolve\n\n")
+	tpl += "Available domain:\n\n"
+	tpl += "%s\n\n"
+	tpl += "Select to show the available %s for that domain.\n\n"
+	tpl += subtle("j/k, up/down: select") + dot + subtle("enter: choose") + dot + subtle("q, esc: quit")
+
+	ch := "%s\n%s\n%s\n"
+	i := 0
+	var checkboxes []any
+	for key, _ := range questionsStore {
+		checkboxes = append(checkboxes, checkbox(key, c == i))
+		i++
+	}
+	choices := fmt.Sprintf(ch, checkboxes...)
+
+	return fmt.Sprintf(tpl, choices, colorFg("calculations", "79"))
+}
+
+// The second view, to show available calculations
+// func calculationsView(m model) string {
+
+// 	tpl := title("Welcome to SciSolve\n\n")
+// 	tpl += "Available calculations:\n\n"
+// 	tpl += "%s\n\n"
+// 	tpl += "Select to perform the %s.\n\n"
+// 	// helper text
+// 	tpl += subtle("j/k, up/down: select") + dot + subtle("enter: choose") + dot + subtle("q, esc: quit")
+
+// 	choices := fmt.Sprintf(
+// 		"%s\n%s\n%s\n%s",
+// 		"math",
+// 		"eofzo",
+// 		"fzioej",
+// 		"fe",
+// 		// checkbox("Math", c == 0),
+// 		// checkbox("Biology", c == 1),
+// 		// checkbox("Physics", c == 2),
+// 		// checkbox("Chemistry", c == 3),
+// 	)
+
+// 	return fmt.Sprintf(tpl, choices, colorFg("calculations", "79"))
+// }
+
+// The third view, after a task has been chosen
+func chosenView(m model) string {
+	var msg string
+
+	switch m.Choice {
+	case 0:
+		msg = fmt.Sprintf("Carrot planting?\n\nCool, we'll need %s and %s...", keyword("libgarden"), keyword("vegeutils"))
+	case 1:
+		msg = fmt.Sprintf("A trip to the market?\n\nOkay, then we should install %s and %s...", keyword("marketkit"), keyword("libshopping"))
+	case 2:
+		msg = fmt.Sprintf("Reading time?\n\nOkay, cool, then we'll need a library. Yes, an %s.", keyword("actual library"))
+	default:
+		msg = fmt.Sprintf("This domain is not implemented yet.\n\nPlease Check later!")
+	}
+
+	return msg + "\n\n"
+}
+
+func checkbox(label string, checked bool) string {
+	if checked {
+		return colorFg("[x] "+label, "212")
+	}
+	return fmt.Sprintf("[ ] %s", label)
 }
 
 func main() {
-	// Create a dictionary of domain/calculators
-	calculators := make(map[string][]string)
-	calculators["Physics"] = []string{
-		"high_pass_filter_calculator",
-	}
-	calculators["Biology"] = []string{
-		"mass",
-		"gravity",
-	}
-	i := 1
-	fmt.Println("Select a domain:")
-	for key := range calculators {
-		fmt.Printf("%d - %s\n", i, key)
-		i++
-	}
-
-	var choice string
-
-	fmt.Scanln(&choice)
-
-	if choice == "1" {
-		choice = "Physics"
-	}
-	if choice == "2" {
-		choice = "Biology"
-	}
-
-	fmt.Printf("Choose a %s formula to calculate:\n", choice)
-	for key, value := range calculators[choice] {
-		fmt.Printf("%d - %s\n", key+1, value)
-	}
-	var formula_idx int
-	for {
-		_, err := fmt.Scanln(&formula_idx)
-		if err == nil && formula_idx > 0 && formula_idx < 2 {
-			break
-		}
-		fmt.Println("Please give a proper answer:")
-	}
-
-	formula := calculators[choice][formula_idx-1]
-
-	switch formula {
-	case calculators[choice][0]:
-		high_pass_filter_calculator()
+	initialModel := model{0, false, false, false, false}
+	p := tea.NewProgram(initialModel)
+	if _, err := p.Run(); err != nil {
+		fmt.Println("could not start program:", err)
 	}
 }
---
+++ utils.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/lucasb-eyer/go-colorful"
+	"github.com/muesli/termenv"
+)
+
+// Utils
+
+// Generate a blend of colors.
+func makeRamp(colorA, colorB string, steps float64) (s []string) {
+	cA, _ := colorful.Hex(colorA)
+	cB, _ := colorful.Hex(colorB)
+
+	for i := 0.0; i < steps; i++ {
+		c := cA.BlendLuv(cB, i/steps)
+		s = append(s, colorToHex(c))
+	}
+	return
+}
+
+// Convert a colorful.Color to a hexadecimal format compatible with termenv.
+func colorToHex(c colorful.Color) string {
+	return fmt.Sprintf("#%s%s%s", colorFloatToHex(c.R), colorFloatToHex(c.G), colorFloatToHex(c.B))
+}
+
+// Helper function for converting colors to hex. Assumes a value between 0 and
+// 1.
+func colorFloatToHex(f float64) (s string) {
+	s = strconv.FormatInt(int64(f*255), 16)
+	if len(s) == 1 {
+		s = "0" + s
+	}
+	return
+}
+
+// Color a string's foreground with the given value.
+func colorFg(val, color string) string {
+	return termenv.String(val).Foreground(term.Color(color)).String()
+}
+
+// Return a function that will colorize the foreground of a given string.
+func makeFgStyle(color string) func(string) string {
+	return termenv.Style{}.Foreground(term.Color(color)).Styled
+}
+
+// Color a string's foreground and background with the given value.
+func makeFgBgStyle(fg, bg string) func(string) string {
+	return termenv.Style{}.
+		Foreground(term.Color(fg)).
+		Background(term.Color(bg)).
+		Bold().
+		Styled
+}
